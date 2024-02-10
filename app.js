50f45e50a4fb82386600cfa1ea4ef2b53cceae4f
kawishali <fayyaz6578@gmail.com>
2024-02-10 22:58:49 +0500

    vercel updated

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const Cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
 
